55cc99a38038222cd9464173789379c0d8c2d399
zhaozhifei <704482973@qq.com>
2020-04-12 18:15:51 +0800

    完成PermissionX库的开发与测试工作

---
+++ library/src/main/java/com/feigor/library/PermissionX.kt
@@ -0,0 +1,19 @@
+package com.feigor.library
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+
+object PermissionX {
+    private const val TAG = "InvisibleFragment"
+    fun request(activity:FragmentActivity,vararg permissions:String, callback: PermissionCallback){
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null){
+            existedFragment as InvisibleFragment
+        }else{
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+}
